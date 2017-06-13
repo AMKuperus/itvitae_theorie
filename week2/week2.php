6eb994f3d04c8acd6e401c6a6b95d11c7e2fa858
AMKuperus <jezeewolde@gmail.com>
2017-06-13 22:44:24 +0200

    Week 2
    + added excursuses for week 2 in TODO list

---
+++ week2/week2.php
@@ -0,0 +1,21 @@
+<?php/*
+//TODO Maak een functie zonder argumenten en pas de functie toe, beschrijf in commentaar
+wat je doet
+//TODO Maak een functie aan met argumenten en pas de functie toe, beschrijf in
+commentaar wat je doet
+//TODO Maak een functie aan met een return value en pas de functie toe, beschrijf in
+commentaar wat je doet
+//TODO Experimenteer met variable scope: Variabelen buiten en binnen een functie, pas ook
+global toe en de $_GLOBAL array, beschrijf in commentaar wat je doet
+//TODO Maak een functie waarbij je parameters uitleest door middel van func_num_args en
+func_get_arg, beschrijf in commentaar wat je doet
+//TODO Maak gebruik van Scalar type declarations in een functies, en pas de functie toe.
+Beschrijf in commentaar wat er gebeurd.
+//TODO Maak gebruik van Return type declarations bij een functie, en pas de functie ook toe.
+Beschrijf in commentaar wat er gebeurd.
+//TODO Pas 'passing by reference' toe bij een functie. Beschrijf in commentaar wat er
+gebeurd.
+//TODO Pas een functie toe met variadics. Beschrijf in commentaar wat er gebeurd.
+//TODO Pas een functie toe met argument unpacking. Beschrijf in commentaar wat er 
+gebeurd.*/
+?>
