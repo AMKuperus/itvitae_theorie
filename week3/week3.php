6108e22fea0b7de9e4602d8dc2d86ce4474af5a0
AMKuperus <jezeewolde@gmail.com>
2017-06-20 13:50:04 +0200

    added prematch

--- week3/week3.php
+++ week3/week3.php
@@ -232,5 +232,16 @@ echo $as[2] . '<br>' . PHP_EOL;
 //Compatible Regular Expressions functies binnen PHP: preg_match, preg_match_all,
 //preg_replace. Beschrijf wederom in comments wat er gebeurd.
 
+//Use preg_match() to test if a string contains digits capitals and normal letters
+//Everything between () is treathed as a group, then ?=.* means "if there is atleast one somewhere"
+//because it is grouped. The [A-Z] looks for capitals [a-z] for small letters and \d for digits (0-9)
+if (preg_match('/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)/', '2abj6876KK', $matches, PREG_OFFSET_CAPTURE)) {
+  echo 'MATCH' . '<br>' . PHP_EOL;
+} else {
+  echo 'NO MATCH' . '<br>' . PHP_EOL;
+}
+//Show the matches
+var_dump($matches);
+
 
 ?>
