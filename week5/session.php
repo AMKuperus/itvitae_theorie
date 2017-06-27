ad2dbda24d4378bd7ac704e13f730b419e95ea35
AMKuperus <jezeewolde@gmail.com>
2017-06-27 18:08:53 +0200

    Update: sepoerated the session excersises from week 5 into seperate file

---
+++ week5/session.php
@@ -0,0 +1,37 @@
+<?php
+//Experimenteer met sessies; de session_start, session_destroy, session_id
+//-functies en de $_SESSION variable
+
+//If constant PHP_SESSION_NONE is the status of session_status there is no
+//session yet and we start one
+if(session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+
+//Check if $_POST['reset'] is set, if yes unset and destroy the session,
+//refresh the page and as extra use exit.
+if(isset($_POST['reset'])) {
+  session_unset();
+  session_destroy();
+  header('Refresh: 0');
+  exit;
+}
+
+//Count number of visits to the page in $_SESSION['visit'], if isset count += 1,
+//else make $_SESSION['visit'] and give it value of 1
+if(isset($_SESSION['visit'])) {
+  $_SESSION['visit'] += 1;
+} else {
+  $_SESSION['visit'] = 1;
+}
+
+//Show $_SESSION['visit']
+echo 'Visited: ' . $_SESSION['visit'] . ' | SID: ' . session_id();
+
+//Form to reset the session
+echo  '<form method="POST">' .
+      '<input type="submit" value="reset" name="reset">' .
+      '</form>';
+
+echo '<a href="http://127.0.0.1/itvitae_theorie/week5/week5.php">BACK to MAIN</a>';
+?>
--- week5/week5.php
+++ week5/week5.php
@@ -1,9 +1,5 @@
 <?php
-//If constant PHP_SESSION_NONE is the status of session_status there is no
-//session yet and we start one
-if(session_status() == PHP_SESSION_NONE) {
-  session_start();
-}
+
 //Pas de header functie toe zoals in de voorbeelden van
 //https://www.w3schools.com/php/func_http_header.asp
 //see index.php for redirect
@@ -17,33 +13,7 @@ if(session_status() == PHP_SESSION_NONE) {
 echo '<a href="http://127.0.0.1/itvitae_theorie/week5/pdf.php" target="_blank">
       CLICK ME TO SEE A PDF</a><br>' . PHP_EOL;
 
-//Experimenteer met sessies; de session_start, session_destroy, session_id
-//-functies en de $_SESSION variable
-
-//Check if $_POST['reset'] is set, if yes unset and destroy the session,
-//refresh the page and as extra use exit.
-if(isset($_POST['reset'])) {
-  session_unset();
-  session_destroy();
-  header('Refresh: 0');
-  exit;
-}
-
-//Count number of visits to the page in $_SESSION['visit'], if isset count += 1,
-//else make $_SESSION['visit'] and give it value of 1
-if(isset($_SESSION['visit'])) {
-  $_SESSION['visit'] += 1;
-} else {
-  $_SESSION['visit'] = 1;
-}
-
-//Show $_SESSION['visit']
-echo 'Visited: ' . $_SESSION['visit'] . ' | SID: ' . session_id();
-
-//Form to reset the session
-echo  '<form method="POST">' .
-      '<input type="submit" value="reset" name="reset">' .
-      '</form>';
+echo '<a href="http://127.0.0.1/itvitae_theorie/week5/session.php">SESSIONS</a>';
 
 //TODO Experimenteer met cookies; setcookie-& setrawcookie-functies. Roep de
 //waarden uit het cookie aan via de $_COOKIE en $_REQUEST variabelen
