affbfbb28ddf317b9f0a1aa62e99b492464f515e
AMKuperus <jezeewolde@gmail.com>
2017-06-27 17:58:35 +0200

    Update: added $_SESSION excersises

--- week5/week5.php
+++ week5/week5.php
@@ -1,18 +1,50 @@
 <?php
+//If constant PHP_SESSION_NONE is the status of session_status there is no
+//session yet and we start one
+if(session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
 //Pas de header functie toe zoals in de voorbeelden van
 //https://www.w3schools.com/php/func_http_header.asp
 //see index.php for redirect
 //Another good explenation on headers is found here:
 //http://www.nicholassolutions.com/tutorials/php/headers.html
 //Turning off caching with header()
-header('Cache-Control: no-cache, no-store, must-revalidate');
-header('Expires: Sun, 01 Jul 2005 00:00:00 GMT');
-header('Pragma: no-cache');
+#Commented out cause of conflict with SESSION
+#header('Cache-Control: no-cache, no-store, must-revalidate');
+#header('Expires: Sun, 01 Jul 2005 00:00:00 GMT');
+#header('Pragma: no-cache');
 echo '<a href="http://127.0.0.1/itvitae_theorie/week5/pdf.php" target="_blank">
-      CLICK ME TO SEE A PDF</a>';
-      
-//TODO Experimenteer met sessies; de session_start, session_destroy, session_id
+      CLICK ME TO SEE A PDF</a><br>' . PHP_EOL;
+
+//Experimenteer met sessies; de session_start, session_destroy, session_id
 //-functies en de $_SESSION variable
+
+//Check if $_POST['reset'] is set, if yes unset and destroy the session,
+//refresh the page and as extra use exit.
+if(isset($_POST['reset'])) {
+  session_unset();
+  session_destroy();
+  header('Refresh: 0');
+  exit;
+}
+
+//Count number of visits to the page in $_SESSION['visit'], if isset count += 1,
+//else make $_SESSION['visit'] and give it value of 1
+if(isset($_SESSION['visit'])) {
+  $_SESSION['visit'] += 1;
+} else {
+  $_SESSION['visit'] = 1;
+}
+
+//Show $_SESSION['visit']
+echo 'Visited: ' . $_SESSION['visit'] . ' | SID: ' . session_id();
+
+//Form to reset the session
+echo  '<form method="POST">' .
+      '<input type="submit" value="reset" name="reset">' .
+      '</form>';
+
 //TODO Experimenteer met cookies; setcookie-& setrawcookie-functies. Roep de
 //waarden uit het cookie aan via de $_COOKIE en $_REQUEST variabelen
 //TODO Pas bij sessies of cookiees ook de serialize & unserialize-functies toe
