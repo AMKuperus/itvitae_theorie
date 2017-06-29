c564af7b9cdcad0ec36e7960c9d3aa4fcf014c17
AMKuperus <jezeewolde@gmail.com>
2017-06-29 13:49:37 +0200

    Update: commented soap

--- week5/soap.php
+++ week5/soap.php
@@ -1,19 +1,24 @@
 <?php
 include 'config.inc.php';
 
-//Soap
+//WSDL is the start string of the webservice to use with soap
 $wsdl = 'https://ws1.webservices.nl/soap_doclit?wsdl';
 
 try {
+  //Make soap object
   $soap = new SoapClient($wsdl);
   $soap->__getFunctions();
+  //Make soap header
   $header = new SoapHeader( 'http://www.webservices.nl/soap/',
                             'HeaderLogin',
                             ['username' => $WSusername, 'password' => $WSpassword],
                             true);
+  //Set soap header
   $soap->__setSoapHeaders($header);
+  //Do a requst on a function and save it to $data
   $data = $soap->addressReeksPostcodeSearch(['address' => '1091GR150']);
 } catch(Exception $e) {
+  //Stop application on errors
   die($e->getMessage());
 }
 var_dump($data);
