3805bc68aa1618079f3a6e350cd8def61e327a63
AMKuperus <jezeewolde@gmail.com>
2017-07-01 14:02:11 +0200

    Adding week 6 excersises

---
+++ week6/week6.php
@@ -0,0 +1,34 @@
+<?php
+//TODO Maak een HTML-formulier aan waarmee je bestanden kan uploaden naar een
+//PHP-script. Zorg dat je met het PHP-script de client-side bestandsnaam, de
+//mime-type enfile-size uitleest en in een bestand zet met dezelfde bestandsnaam,
+//alleen dan met .txt op het einde als extensie.
+
+//TODO Maak een tekstbestand aan via de f*-functies (zoals fopen, etc). Bewerk
+//dit tekstbestand later in de code; voeg ‘Last edited on <datum>’ toe aan het
+//einde van het bestand. Toon vervolgens de inhoud van het bestand op het scherm.
+
+//TODO Vraag de inhoud van het tekstbestand op via de file_get_content-functie,
+//en overschrijf de inhoud met de file_put_content-functie
+
+//TODO Maak een PHP-script aan om in een CSV-bestand een array te schrijven
+
+//TODO Pas de fpassthru en fprintf-functies toe
+
+//TODO Pas de file operation functies toe: chdir, chroot, readdir, rmdir, basename,
+//chmod, copy, file_exists, fputs, rename, unlink. Beschrijf in commentaar welke functies niet
+//werkten.
+
+//TODO Pas deze functies toe om een bestand/map na te checken: is_dir, is_executable,
+//is_file, is_link, is_readable, is_writable, is_uploaded_file.
+
+//TODO Haal een webpagina op met je eigen PHP script.
+
+//TODO Vraag informatie over een bestand op via de finfo_open en finfo_file functies
+
+//TODO Maak een simpele client/server applicatie met de stream_socket_server en
+//stream_socket_client functies.
+
+//TODO Pas stream filters toe in je client/server applicatie.
+
+?>
