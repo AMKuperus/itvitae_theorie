e8836ecfaeeeb9b6b97a1f2cc207eb57de03d0bd
AMKuperus <jezeewolde@gmail.com>
2017-07-04 19:31:33 +0200

    Adding week 7 excersises

---
+++ week7/week7.php
@@ -0,0 +1,34 @@
+<?php
+//TODO Ontwikkel een applicatie waarmee je records van een relationele database
+//kan bekijken, toevoegen, bewerken, verwijderen. Gebruik hiervoor PDO of de
+//MySQLi functies. De relationele database bevat minimaal 4 tabellen en pas alle
+//soorten relaties toe: one-to-one, one-to-many, en many-to-many.
+
+//TODO Pas in je database tabellen zowel numerieke als string data types toe.
+
+//TODO Pas ook verschillende datum/tijden toe in je tabellen; deze worden
+//volgende week weer gebruik. Denk bijv aan een geboortedatum, ‘registered date’
+//en een ‘last login date’.
+
+//TODO Experimenteer met SELECT, INSERT, UPDATE, DELETE SQL queries met de andere
+//techniek dan je hierboven hebt gekozen (dus PDO of de MySQLi functies).
+
+//TODO Zorg er voor dat je ook prepared statements toe pas.
+
+//TODO Pas bij de SELECT queries ook ORDER BY, en GROUP BY toe
+
+//TODO Experimenteer bij SELECT queries ook met AVG, COUNT, DISTINCT COUNT, MIN,
+//MAX en SUM. Mag in SQL-file gezet worden, maar ook via een PHP-script.
+
+//TODO Schrijf een CREATE DATABASE & CREATE TABLE SQL querie voor de database van
+//je applicatie. Mag in SQL-file gezet worden, maar ook via een PHP-script.
+
+//TODO Schrijf een SQL DROP query voor zowel table als schema.
+
+//TODO Pas PRIMARY KEY & FOREIGN KEY toe in de tabellen van je relationele database
+
+//TODO Experimenteer met een SQL INNER JOIN , LEFT JOIN en RIGHT JOIN
+
+//TODO Experimenteer met SQL TRANSACTIONS
+
+?>
