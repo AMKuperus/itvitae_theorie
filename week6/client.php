3bc25d8f897c3c75933a8fb9bc9772109a886baa
AMKuperus <jezeewolde@gmail.com>
2017-07-05 10:22:18 +0200

    Update client-server-streams excersise

--- week6/client.php
+++ week6/client.php
@@ -1,8 +1,12 @@
 <?php
+//Create socket for client
   $socket = stream_socket_client('tcp://127.0.0.1:8082');
+  //Do somethin while feof() is not empty
   while (!feof($socket)) {
     echo fread($socket, 100);
     echo 'Hello world';
   }
+  //Close the socket
   fclose($socket);
+  exit;
 ?>
--- week6/server.php
+++ week6/server.php
@@ -1,19 +1,26 @@
 <?php
-
+//Open a connection with stream_socket_server()
 $server = stream_socket_server('tcp://127.0.0.1:8082', $errno, $errorMessage);
+//Set a counter to count #visits
 $count = 0;
 
+//If server is not there spit out a error
 if ($server === FALSE) {
   throw new UnexpectedValueException('Unable to bind socket: ' . $errorMessage);
 }
 
+//Accept a stream (from client)
 for (;;) {
   $client = stream_socket_accept($server);
+  //Count++
   $count++;
+  //Apply a rot13 filter over all text that was written in this file
+  //The input from client will stay clean and without filter
+  //The filter works on fwrite() in the is-statement
+  stream_filter_prepend($client, 'string.rot13');
   echo 'Connection @ ' . date('H:i:s') . ' | Visits: ' . $count . PHP_EOL;
   if ($client) {
-
-    fwrite($client, date('H:i:s'));
+    fwrite($client, 'Time: ' . date('H:i:s'));
     stream_copy_to_stream($client, $client);
     fclose($client);
   }
