40255a0fc3bc19b3dca69c76514d7dee52268cc3
AMKuperus <jezeewolde@gmail.com>
2017-07-14 22:38:13 +0200

    FormBuilder class
    + added method to add text input field
    + added title to elements and set them inbetween <p>

--- week7/cat4rent/FormBuilder.class.php
+++ week7/cat4rent/FormBuilder.class.php
@@ -1,8 +1,9 @@
 <?php
 //    --------------- A class to easily build forms ----------------
-//    by AMKuperus | Version: 0.2Beta | Last revision: 14 July 2017
 //    Do not alter or reproduce without the knowledge of the author.
 //    Contains the basic most used html-form-elements and attributes.
+//              TODO add more input options in the future
+
 namespace Form;
 
 class FormBuilder {
@@ -30,25 +31,31 @@ class FormBuilder {
     $this->start[] = "<fieldset><legend>$legend</legend>\n";
     $this->endFieldset();
   }
-  //TODO input (set type, set required, set value, set name)
+  //Add a text-input HTML element
+  //$title is the title displayed in front of the textfield | $name is the name
+  //$required and $placeholder are optional, however if you need placeholder and
+  //don't need required the fill in '' for required when calling method.
+  public function inputText($title, $name, $required = '', $placeholder = '') {
+    $this->start[] = "<p>$title<input type=\"text\" name=\"$name\" placeholder=\"$placeholder\" $required></p>\n";
+  }
 
   //Create a select html attribute with dropdownlist containing options
-  //$name = name
+  //$title is name in front of input field | $name = name
   //In order to use attributes multiple and required fill in $multiple and $required
   //with coresponding values.
   //value $values should contain a associative array containing key-value pairs
   //key becomes value-attribute and value becomes the visible name in option.
   //$multiple and $required are optional, however if not required is not needed but
-  //multiple is required should be added as '' in function-call.
-  public function createSelect($name, $values, $required = '', $multiple = '') {
+  //multiple is required should be added as '' in method-call.
+  public function select($title, $name, $values, $required = '', $multiple = '') {
     //Create the <select>
-    $this->content[] = "<select name=\"$name\" $required $multiple>\n";
+    $this->content[] = "<p>$title<select name=\"$name\" $required $multiple>\n";
     //Create the options from array with foreach
     foreach($values as $k => $v) {
       $this->content[] = "<option value=\"$k\">$v</option>\n";
     }
     //Create end for select
-    $this->content[] = "</select>\n";
+    $this->content[] = "</select></p>\n";
   }
 
   //Add submit button with optional $value
