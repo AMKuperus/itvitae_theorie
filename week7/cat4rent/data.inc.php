2edc66642983873037b55fd15b4f6d4be682847a
AMKuperus <jezeewolde@gmail.com>
2017-07-14 13:23:42 +0200

    Update cat 4 rent
    - Cleaned up code: removed old code that was no longer needed

--- week7/cat4rent/data.inc.php
+++ week7/cat4rent/data.inc.php
@@ -10,48 +10,6 @@ try {
   die();
 }
 
-/*<--DEPRECATED function showCats() {
-  global $db;
-  $sql = "SELECT * FROM cats";
-
-  $res = $db->query($sql);
-  $res->setFetchMode(PDO::FETCH_OBJ);
-
-  echo '<table>' .
-        '<tr>' .
-          '<th>ID</th>' .
-          '<th>Name</th>' .
-          '<th>Color</th>' .
-          '<th>Type</th>' .
-          '<th>Price</th>' .
-          '<th>Status</th>' .
-        '</tr>';
-  foreach ($res as $r) {
-    echo "<tr>
-          <td>{$r->cat_id}</td>
-          <td>{$r->name}</td>
-          <td>{$r->color}</td>
-          <td>{$r->type} </td>
-          <td>{$r->price}</td>
-          <td>{$r->status}</td>
-          </tr>\n";
-  }
-  echo '</table>';
-}DEPRECATED-->*/
-
-/*<--DEPRECATEDfunction addCat($name, $color, $type, $price, $status) {
-  global $db;
-  $sql = "INSERT INTO cats (name, color, type, price, status) VALUES
-                          (:name, :color, :type, :price, :status)";
-  $do = $db->prepare($sql);
-  $do->bindValue(':name', $name, PDO::PARAM_STR);
-  $do->bindValue(':color', $color, PDO::PARAM_STR);
-  $do->bindValue(':type', $type, PDO::PARAM_STR);
-  $do->bindValue(':price', $price, PDO::PARAM_STR);
-  $do->bindValue(':status', $status, PDO::PARAM_STR);
-  $do->execute();
-}-->DEPRECATED*/
-
 function showCustomers() {
   global $db;
   $sql = "SELECT * FROM customers";
--- week7/cat4rent/functions.inc.php
+++ week7/cat4rent/functions.inc.php
@@ -9,46 +9,4 @@ function showMenu() {
         '<a href="addCustomer.php">Add Customer</a></p>';
 }
 
-/*<--DEPRECATEDfunction addCatForm() {
-  echo '<form method="POST">
-        <fieldset><legend>Add a cat</legend>
-        <p>Name: <input type="text" name="name" required></p>
-        <p>Color: <input type="text" name="color" required></p>
-        <p>Type: <input type="text" name="type" required></p
-        <p>Price: <select name="price" required>
-                    <option value="A">A</option>
-                    <option value="B">B</option>
-                    <option value="C">C</option>
-                    <option value="D">D</option>
-                    <option value="E">E</option>
-                </select></p>
-        <p>Status: <select name="status" required>
-                    <option value="available">Available</option>
-                    <option value="unavailable">Unavailable</option>
-                </select>
-        <input type="submit">
-        </fieldset></form>';
-}-->DEPRECATED*/
-
-function actionAddCat() {
-  if(isset($_POST['name'])) {
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['color'])) {
-    $color = filter_var($_POST['color'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['type'])) {
-    $type = filter_var($_POST['type'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['price'])) {
-    $price = filter_var($_POST['price'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['status'])) {
-    $status = filter_var($_POST['status'], FILTER_SANITIZE_STRING);
-  }
-  echo "$name | $color | $type | $price | $status";
-  addCat($name, $color, $type, $price, $status);
-  echo '<p>Succesfully added: ' . $name . ' to the database</p>';
-}
-
 ?>
