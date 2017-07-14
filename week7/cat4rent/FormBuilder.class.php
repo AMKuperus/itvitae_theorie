d9cab2e5422852ad5e96e4fc0773a1773292903a
AMKuperus <jezeewolde@gmail.com>
2017-07-14 21:38:49 +0200

    Update: adding FormBuilder class
    FormBuilder class makes it easier to create forms in php-script.
    FormBuilder becomes a reusable class for codeStash in the future.

---
+++ week7/cat4rent/FormBuilder.class.php
@@ -0,0 +1,81 @@
+<?php
+//A class to easily build forms
+namespace Form;
+
+class FormBuilder {
+  private $start = [];
+  private $content = [];
+  private $end = [];
+
+  //TODO start form
+  //Use following example $options[] associative array and fill in desired options
+  //All options you do not want to use you should leave blank.
+  /*  $options = [ 'name' => '',
+      	           'method' => 'POST',
+                   'action' => '',
+                   'enctype' => '',
+                   'target' => ''];                                           */
+  public function startForm($options) {
+    $this->start[] = "<form name=\"{$options['name']}\" method=\"{$options['method']}\"
+                      action=\"{$options['action']}\" enctype=\"{$options['enctype']}\"
+                      target=\"{$options['target']}\">\n";
+  }
+
+  //Add fieldset with optional title $legend
+  //$legend represents title of fieldset that shows in the fieldset
+  public function startFieldset($legend = '') {
+    $this->start[] = "<fieldset><legend>$legend</legend>\n";
+    $this->endFieldset();
+  }
+  //TODO input (set type, set required, set value, set name)
+
+  //Create a select html attribute with dropdownlist containing options
+  //$name = name
+  //In order to use attributes multiple and required fill in $multiple and $required
+  //with coresponding values.
+  //value $values should contain a associative array containing key-value pairs
+  //key becomes value-attribute and value becomes the visible name in option.
+  //$multiple and $required are optional, however if not required is not needed but
+  //multiple is required should be added as '' in function-call.
+  public function createSelect($name, $values, $required = '', $multiple = '') {
+    //Create the <select>
+    $this->content[] = "<select name=\"$name\" $required $multiple>\n";
+    //Create the options from array with foreach
+    foreach($values as $k => $v) {
+      $this->content[] = "<option value=\"$k\">$v</option>\n";
+    }
+    //Create end for select
+    $this->content[] = "</select>\n";
+  }
+
+  //Add submit button with optional $value
+  //$value represents the name on the button
+  public function submit($value = '') {
+    $this->content[] = "<input type=\"submit\" value=\"$value\">\n";
+  }
+  //End the fieldset
+  private function endFieldset() {
+    $this->end[] = "</fieldset>\n";
+  }
+  //End the form
+  public function endForm() {
+    $this->end[] = "</form>\n";
+  }
+
+  //Publish the form
+  public function publish() {
+    //Publish the beginning of the form
+    foreach($this->start as $v) {
+      echo "$v";
+    }
+    //Publish the content section of the form
+    foreach($this->content as $c) {
+      echo "$c";
+    }
+    //Publish the end of the form
+    foreach($this->end as $end) {
+      echo "$end";
+    }
+  }
+
+}
