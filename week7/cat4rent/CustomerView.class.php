62ab95f3eac05ec5f157307e145dea5115f0b5f8
AMKuperus <jezeewolde@gmail.com>
2017-07-26 12:50:44 +0200

    cat4rent - CustomerView.class
    + showCustomer()
    + showCustomerTable()
    + showAddCustomerForm()
    - all methods still need testing!

--- week7/cat4rent/CustomerView.class.php
+++ week7/cat4rent/CustomerView.class.php
@@ -8,8 +8,51 @@ use Tools\TableBuilder;
 use Tools\FormBuilder;
 
 class CustomerView {
-  //TODO showCustomer
-  //TODO showCustomerTable
-  //TODO showAddCustomerForm
+  /**
+   * Show information about specific customer.
+   * @param  Object   $customerObj  Customer-object
+   * @return String                 <div> with customer-info.
+   */
+  public static function showCustomer($customerObj) {
+    $customerInfo = '<div><p>Name: ' . $customerObj->fname . ' ' .
+                    $customerObj->lname . '</p>' .
+                    '<p>ID: ' . $customerObj->id .
+                    ' | Behaviourcode: ' . $customerObj->behaviour .
+                    '</p></div>';
+    return $customerInfo;
+  }
+
+  /**
+   * Create a table that shows all the customers present in teh database.
+   * @param  Array   $array   Array containing all customer-objects
+   * @return Object           TableBuilder-object
+   */
+  public static function showCustomersTable($array) {
+    $table = new TableBuilder();
+    $head = ['ID', 'First name', 'Last name', 'Behaviour code'];
+    $table->tableHead($head, 'All our customers');
+    $items = ['id', 'fname', 'lname', 'behaviour'];
+    $table->tableBody($items, $array);
+    $table->tableFoot();
+    $table->publish();
+  }
+
+  /**
+   * Create a form to add new customers to the database.
+   * @return Object           FormBuilder-object
+   */
+  public static function showAddCustomerForm() {
+    $formOptions = ['name' => 'addCustomerForm', 'method' => 'POST', 'action' => '',
+                    'enctype' => '', 'target' => ''];
+    $form = new FormBuilder();
+    $form->startForm($formOptions);
+    $form->startFieldset('Regiter a customer');
+    $form->inputText('First name: ', 'fname', 'required', 'firts name');
+    $form->inputText('Last Name: ', 'lname', 'required', 'last name');
+    $form->submit('Register');
+    $form->endForm();
+    $form->publish();
+  }
+
 }
 ?>
