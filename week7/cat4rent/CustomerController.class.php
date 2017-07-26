97414b5e4b6e04858ba17e03ea13a921dd55e615
AMKuperus <jezeewolde@gmail.com>
2017-07-26 13:09:10 +0200

    cat4rent CustomerController.class
    + viewCustomers()
    + viewAddCustomer()
    + getAllCustomers()
    - Still needs testing!

--- week7/cat4rent/CustomerController.class.php
+++ week7/cat4rent/CustomerController.class.php
@@ -12,10 +12,43 @@ class CustomerController {
     $this->db = $db;
   }
 
-  //TODO viewCustomers
-  //TODO viewAddCustomer
+  /**
+   * Show all the customers in a table.
+   * @return Object   TableBuilder-object
+   */
+  public function viewCustomers() {
+    $arrCustomers = $this->getAllCustomers();
+    CustomerView::showCustomersTable($arrCustomers);
+  }
+
+  /**
+   * Show a form to add a customer.
+   * @return Object  FormBuilder-object
+   */
+  public function viewAddCustomer() {
+    CustomerView::showAddCustomerForm();
+  }
+
   //TODO getCustomer
-  //TODO getAllCustomers
+
+  /**
+   * Get all the customers from the database and create Customers-objects for
+   * each customer.
+   * @return Array   Array containing all Customer-objects.
+   */
+  public function getAllCustomers() {
+    $sql = "SELECT * FROM customers";
+    $ask = $this->db->prepare($sql);
+    $ask->execute();
+    $result = $ask->fetchAll();
+    $arrCustomers = [];
+    foreach ($result as $r) {
+      $objCustomer = new Customer($r);
+      $arrCustomers[] = $objCustomer;
+    }
+    return $arrCustomers;
+  }
+
   //TODO checkCustomer
   //TODO addCustomer
   //TODO updateBehaviour
